d73970e87348734ad5d8dd99c06281488f9f7975
Lucas <lucasgstumpf@gmail.com>
2021-08-16 16:54:28 -0300

    Corrigido erros, adicionado novas funcoes

--- Interface.py
+++ Interface.py
@@ -1,6 +1,14 @@
-# aumentar base e distancia entre as torres
+"""
+-Colocar Lista de jogadas aparecendo
+-Texto para torre entrada e torre saida e botao para confirmar
+-Enfeites
+-Botao Voltar jogada
 
-#import TorreHanoi
+"""
+
+
+import TorreHanoi
+from TorreHanoi import *
 import tkinter as tk
 from tkinter import *
 
@@ -26,7 +34,7 @@ torreInterface3=Label(app,background = "#808080",foreground ="#000")
 torreInterface3.place(x=950,y=50,width="10",height="300")
 
 base= Label(app,background = "#000",foreground ="#000")
-base.place(x=50,y=350,width="1000",height="30")
+base.place(x=30,y=350,width="1100",height="30")
 
 #-------------------------DISCOS-------------------------
 
@@ -35,74 +43,68 @@ base.place(x=50,y=350,width="1000",height="30")
 #o X deve ser mudado para x = (numero padrao) + (Torre * 400)
 #
 
-disco1 = Label(app,background = "#ff0",foreground ="#000")
+disco6 = Label(app,background = "#ff0",foreground ="#000")
 #disco1.place(x=117,y=170,width="75",height="30")
 
-disco2 = Label(app,background = "#f23",foreground ="#000")
+disco5 = Label(app,background = "#f23",foreground ="#000")
 #disco2.place(x=103,y=200,width="105",height="30")
 
-disco3 = Label(app,background = "#103",foreground ="#000")
+disco4 = Label(app,background = "#103",foreground ="#000")
 #disco3.place(x=89,y=230,width="135",height="30")
 
-disco4 = Label(app,background = "#080",foreground ="#000")
+disco3 = Label(app,background = "#080",foreground ="#000")
 #disco4.place(x=75,y=260,width="165",height="30")
 
-disco5 = Label(app,background = "#ff007f",foreground ="#000")
+disco2 = Label(app,background = "#ff007f",foreground ="#000")
 #disco5.place(x=61,y=290,width="195",height="30")
 
-disco6 = Label(app,background = "#0000ff",foreground ="#000")
+disco1 = Label(app,background = "#0000ff",foreground ="#000")
 #disco6.place(x=47,y=320,width="225",height="30")
 
 def UIInicializarDiscos(totalDiscos):
     aux = 0
     if aux < totalDiscos:
-        disco6.place(x=47,y=320,width="225",height="30")
+        disco1.place(x=47,y=320,width="225",height="30")
         aux = aux + 1
     if aux < totalDiscos:
-        disco5.place(x=61,y=290,width="195",height="30")
+        disco2.place(x=61,y=290,width="195",height="30")
         aux = aux + 1
     if aux < totalDiscos:
-        disco4.place(x=75,y=260,width="165",height="30")
+        disco3.place(x=75,y=260,width="165",height="30")
         aux = aux + 1
     if aux < totalDiscos:
-        disco3.place(x=89,y=230,width="135",height="30")
+        disco4.place(x=89,y=230,width="135",height="30")
         aux = aux + 1
     if aux < totalDiscos:
-        disco2.place(x=103,y=200,width="105",height="30")
+        disco5.place(x=103,y=200,width="105",height="30")
         aux = aux + 1
     if aux < totalDiscos:
-        disco1.place(x=117,y=170,width="75",height="30")
+        disco6.place(x=117,y=170,width="75",height="30")
 
-total = int(input("escreva: "))
-UIInicializarDiscos(total)
-
-"""
 # FUNÇÃO PARA MOVER OS DISCOS
 #Precisa receber o numero do disco que quer sair, e precisa saber qual torre vai e qual posição,
 #Precisa saber a posição no array q ele vai entrar( len()-1)
 #Criar uma função que ve a altura do negocio
 
 def UIMoverDisco(disco,torreEscolha,altura):
-    if disco == 1:
-        disco1.place(x= (117+((torreEscolha - 1)*400)) , y=  170 + (30* altura) ,width="75",height="30")
+    if disco == 6:
+        disco6.place(x= (117+((torreEscolha - 1)*400)),y= 320 - (30* altura) ,width="75",height="30")
    
-    elif disco == 2:
-        disco2.place(x=103+(((torreEscolha - 1)*400)),y=200 + (30* altura) ,width="105",height="30")
-
-    elif disco == 3:
-        disco3.place(x=89 +(((torreEscolha - 1)*400)),y=230 + (30* altura) ,width="135",height="30")
+    elif disco == 5:
+        disco5.place(x=103+(((torreEscolha - 1)*400)),y= 320 - (30* altura) ,width="105",height="30")
 
     elif disco == 4:
-        disco4.place(x=75 +(((torreEscolha - 1)*400)),y=260 + (30* altura),width="165",height="30")
+        disco4.place(x=89 +(((torreEscolha - 1)*400)),y=320 - (30* altura) ,width="135",height="30")
 
-    elif disco == 5:
-        disco5.place(x=61 +(((torreEscolha - 1)*400)),y=290 + (30* altura),width="195",height="30")
+    elif disco == 3:
+        disco3.place(x=75 +(((torreEscolha - 1)*400)),y=320 - (30* altura),width="165",height="30")
 
-    elif disco == 6:
-        disco6.place(x=47 +(((torreEscolha - 1)*400)),y=320 + (30* altura),width="225",height="30")
+    elif disco == 2:
+        disco2.place(x=61 +(((torreEscolha - 1)*400)),y=320 - (30* altura),width="195",height="30")
 
-def altura()
-"""
+    elif disco == 1:
+        disco1.place(x=47 +(((torreEscolha - 1)*400)),y=320 - (30* altura),width="225",height="30")
 
+TorreHanoi.main()
 
 app.mainloop()
--- TorreHanoi.py
+++ TorreHanoi.py
@@ -1,55 +1,136 @@
+
+import Interface
+from Interface import *
+
 #------------------
 def Teste():
     x = 5 
     return x
 
-
-
+#-------------------Resolução Recursiva
+def resolucaoRecursiva(totalDiscos, fromPole, toPole, withPole, torre1, torre2, torre3, jogadas):
+    if totalDiscos >= 1:
+        resolucaoRecursiva(totalDiscos-1,fromPole,withPole,toPole, torre1, torre2, torre3, jogadas)
+        trocaDeTorre(fromPole, toPole, torre1, torre2, torre3, jogadas)
+        resolucaoRecursiva(totalDiscos-1,withPole,toPole,fromPole, torre1, torre2, torre3, jogadas)
 
 #------------------- Lista de jogadas
 def ListaJogadas(jogadas,torreSaida,torreEscolha ):
     jogadas.append([torreSaida, torreEscolha])
 
 #-------------------Voltar Jogadas
-
 def VoltarJogadas(jogadas, torre1, torre2, torre3):
     ultima = int(len(jogadas) - 1)
     torreEscolha = int(jogadas[ultima][0])
     torreSaida = int(jogadas[ultima][1]) 
-    trocaDeTorre(torreSaida, torreEscolha, torre1, torre2, torre3)
+    trocaDeTorre(torreSaida, torreEscolha, torre1, torre2, torre3, jogadas)
 
 
 #-------------------Verifica se ganhou o jogo
 def WinTeste(torre0,torre2,torre3):
-
     if (torre2 == torre0) or (torre3 == torre0):
         return True
     else:
         return False
 
-#-------------------Realiza a troca do disco nas torres
-def trocaDeTorre(torreSaida, torreEscolha, torre1, torre2, torre3):
+#------------------Compara discos
+def comparaDisco(torreSaida, torreEscolha, torre1, torre2, torre3):
+    certo = False
     if torreSaida == 1:
         if torreEscolha == 2:
+            discoSaida = len(torre1)
+            discoEscolha = len(torre2)
+            if discoEscolha == 0:
+                certo = True
+                return certo
+            elif torre1[discoSaida-1] > torre2[discoEscolha-1]:
+                certo = True
+        elif torreEscolha == 3:
+            discoSaida = len(torre1) - 1
+            discoEscolha = len(torre3)
+            if discoEscolha == 0:
+                certo = True
+                return certo
+            elif torre1[discoSaida-1] > torre3[discoEscolha-1]:
+                certo = True
+            
+    
+    elif torreSaida == 2:
+        if torreEscolha == 1:
+            discoSaida = len(torre2) - 1
+            discoEscolha = len(torre1)
+            if discoEscolha == 0:
+                certo = True
+                return certo
+            elif torre2[discoSaida-1] > torre1[discoEscolha-1]:
+                certo = True
+
+        elif torreEscolha == 3:
+            discoSaida = len(torre2) - 1
+            discoEscolha = len(torre3)
+            if discoEscolha == 0:
+                certo = True
+                return certo
+            elif torre2[discoSaida-1] > torre3[discoEscolha-1]:
+                certo = True
+ 
+    elif torreSaida == 3:
+        if torreEscolha == 1:
+            discoSaida = len(torre3) - 1
+            discoEscolha = len(torre1)
+            if discoEscolha == 0:
+                certo = True
+                return certo
+            elif torre3[discoSaida-1] > torre1[discoEscolha-1]:
+                certo = True
+
+        elif torreEscolha == 2:
+            discoSaida = len(torre3) - 1
+            discoEscolha = len(torre2)
+            if discoEscolha == 0:
+                certo = True
+                return certo
+            elif torre3[discoSaida-1] > torre2[discoEscolha-1]:
+                certo = True
+                          
+    return certo
+
+#-------------------Realiza a troca do disco nas torres
+def trocaDeTorre(torreSaida, torreEscolha, torre1, torre2, torre3, jogadas):
+    novoDisco = 0
+    altura = 0
+    if torreSaida == 1:
+        if torreEscolha == 2:
+            altura = len(torre2)
             novoDisco = retirarTorre(torre1)
             acrescentarTorre(torre2,novoDisco)
         elif torreEscolha == 3:
+            altura = len(torre3)
             novoDisco = retirarTorre(torre1)
             acrescentarTorre(torre3,novoDisco)
+
     elif torreSaida == 2:
         if torreEscolha == 1:
+            altura = len(torre1)
             novoDisco = retirarTorre(torre2)
             acrescentarTorre(torre1,novoDisco)
         elif torreEscolha == 3:
+            altura = len(torre3)
             novoDisco = retirarTorre(torre2)
             acrescentarTorre(torre3,novoDisco)
+  
     elif torreSaida == 3:
         if torreEscolha == 1:
+            altura = len(torre1)
             novoDisco = retirarTorre(torre3)
             acrescentarTorre(torre1,novoDisco)
         elif torreEscolha == 2:
+            altura = len(torre2)
             novoDisco = retirarTorre(torre3)
             acrescentarTorre(torre2,novoDisco)
+
+    Interface.UIMoverDisco(novoDisco, torreEscolha, altura)
+    
 
 #------------------Implementação da Pilha
 def acrescentarTorre(torre, disco):
@@ -87,17 +168,23 @@ def main():
     torre2 = []
     torre3 = []
     jogadas = []
+    i = 1
+    Interface.UIInicializarDiscos(totalDiscos)
 
-    
+
     #Acrescenta todos os discos na torre 1
-    while totalDiscos > 0:
-        acrescentarTorre(torre1,totalDiscos)
-        acrescentarTorre(torre0,totalDiscos)
-        totalDiscos -= 1
+    while i <= totalDiscos:
+        acrescentarTorre(torre1,i)
+        acrescentarTorre(torre0,i)
+        i += 1
 
+    totalDiscos = len(torre1)
+
+    print("total discos: ", totalDiscos)
     print(torre1)
     print(torre2)
     print(torre3)
+    
     saiu = False
     while not saiu:
         aux = 0
@@ -108,24 +195,30 @@ def main():
             torreSaida = int(input("De qual torre deseja retirar o disco?(1,2,3) "))
             torreEscolha = int(input("Para qual torre irá o disco?(1,2,3,) "))
 
-            ListaJogadas(jogadas,torreSaida,torreEscolha) #Armazena as jogadas
 
             if torreSaida != torreEscolha:
                 aux == 1
+
             if not (torreSaida < 4 and torreSaida > 0 and torreEscolha < 4 and torreEscolha > 0):
                 print("\nEscolha de torre inadequada\n")
-            
 
-
-        #Realiza a troca do disco nas torres
-        trocaDeTorre(torreSaida, torreEscolha, torre1, torre2, torre3)
-        saiu = WinTeste(torre0, torre2, torre3)
+        #Realiza a troca do disco nas torres]
+        if comparaDisco(torreSaida, torreEscolha, torre1, torre2, torre3):
+            trocaDeTorre(torreSaida, torreEscolha, torre1, torre2, torre3,jogadas)
+            ListaJogadas(jogadas, torreSaida, torreEscolha)
+            saiu = WinTeste(torre0, torre2, torre3)
 
         print(torre1)
         print(torre2)
         print(torre3)
         print(jogadas)
+        
+        VoltarJogadas(jogadas,torre1,torre2,torre3)
+
+        print(torre1)
+        print(torre2)
+        print(torre3)
+        print(jogadas)
+     
 
     print("Parabéns você venceu")
-
-main()
